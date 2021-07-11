02dc1951528dfb46c01745b96988878147eb0692
elsa-k-donovan <elsa.k.donovan@gmail.com>
2021-07-11 19:19:48 +0200

    Update frontend_config.py

--- DDI_Website/Website_Settings/frontend_config.py
+++ DDI_Website/Website_Settings/frontend_config.py
@@ -2,8 +2,8 @@ Django = {
     "Key": 'j5dusp=dv+dec^q#$w2n8-e!fvm46$s!8s@9673o(l#efmvh-e',
 }
 PostgreSQL = {
-    "UserName": "edonovan",
-    "Password": "MICEMICE9",
+    "UserName": "",
+    "Password": "",
     "Name": "test",
     "Host": "localhost",
     "Port": 5432,
